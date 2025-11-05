9a62b9d9e79551ed0bdb05407edc79ccfe27eca4
Belchatina <isabellos385@gmail.com>
2025-11-05 21:34:28 +0700

    Add picture loading to random-photos project

--- random-photos/index.js
+++ random-photos/index.js
@@ -1,16 +1,31 @@
 const imageContainerEl = document.querySelector('.image-container');
 const btnEl = document.querySelector('.btn');
 
+addNewImage(5);
+
 btnEl.addEventListener('click', () => {
-  addNewImages(10);
+  addNewImage(5);
 });
 
-function addNewImages(imageNum) {
+function createPlaceholder() {
+  LOAD_URL =
+    'https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMXk5emxvd2hscWF3NTRndjg0NjY5YzFyMnhwcHU5Z3BoY2FsYmpycCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/HMSLfCl5BsXoQ/giphy.gif';
+  const placeholderImg = document.createElement('img');
+  placeholderImg.src = LOAD_URL;
+  placeholderImg.style.objectFit = 'cover';
+  return placeholderImg;
+}
+
+function addNewImage(imageNum) {
   for (let index = 0; index < imageNum; index++) {
+    const placeholderImg = createPlaceholder();
+    imageContainerEl.appendChild(placeholderImg);
     const newImgEl = document.createElement('img');
     newImgEl.src = `https://picsum.dev/300/300?random=${Math.floor(
       Math.random() * 2000
     )}`;
-    imageContainerEl.appendChild(newImgEl);
+    newImgEl.addEventListener('load', () => {
+      imageContainerEl.replaceChild(newImgEl, placeholderImg);
+    });
   }
 }
